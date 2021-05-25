cbaad61cf417d0d14a398fc27348fe96496f1989
g-weiher <74683223+g-weiher@users.noreply.github.com>
2021-05-25 08:33:29 +0200

    component cleanup

--- components/side-menu/index.js
+++ components/side-menu/index.js
@@ -1,44 +1,50 @@
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
 import MenuButton from "./components/MenuButton";
 import ArrowButton from "./components/arrowButton";
 import { useState } from "react";
 import { useScrollPosition } from "../../hooks/useScrollPositionCustom";
 
-const SideMenu = ({children}) => {
+const SideMenu = ({ children }) => {
     const [isScrolled, setScrolled] = useState(false);
     const [showMenu, setShowMenu] = useState(false);
     useScrollPosition(
-        ({currPos}) => {
+        ({ currPos }) => {
             console.log(currPos);
             console.log(isScrolled);
-          const isShow = currPos.y < -100;
-          if (isShow !== isScrolled) setScrolled(isShow);
+            const isShow = currPos.y < -100;
+            if (isShow !== isScrolled) setScrolled(isShow);
         },
         [isScrolled],
         false,
         false,
         300
-    )
+    );
     const onToggleMenu = () => {
         setShowMenu(!showMenu);
-    }
+    };
     return (
         <header className={styles.header}>
             <div id="sideMenu" className={styles.sidebar}>
                 <div>
-                    <MenuButton onClick={()=> onToggleMenu()}/>   
-                    {isScrolled ? <ArrowButton up/> : <ArrowButton up hidden/>}
+                    <MenuButton onClick={() => onToggleMenu()} />
+                    {isScrolled ? (
+                        <ArrowButton up />
+                    ) : (
+                        <ArrowButton up hidden />
+                    )}
                 </div>
-                {!isScrolled ? <ArrowButton down/> : <ArrowButton down hidden/>}
+                {!isScrolled ? (
+                    <ArrowButton down />
+                ) : (
+                    <ArrowButton down hidden />
+                )}
             </div>
-            <div className={`${styles.drawer} ${showMenu ? styles.active: ""}`}>
-                <nav>
-                    {children}
-                </nav>
-                
+            <div
+                className={`${styles.drawer} ${showMenu ? styles.active : ""}`}
+            >
+                <nav>{children}</nav>
             </div>
-
         </header>
-    )
-}
+    );
+};
 export default SideMenu;
